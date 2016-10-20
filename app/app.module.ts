2e769727631ae932cee20973c6370c894d778c66
Prog2 <prog2@consysonline.com.br>
2016-10-20 17:55:05 -0200

    Tour of heroes v2 commit

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,13 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 
 import { AppComponent }  from './app.component';
+import { HeroComponent } from './hero.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
 
 @NgModule({
    imports: [
       BrowserModule,
       FormsModule 
    ],
-   declarations: [ AppComponent ],
-   bootstrap: [ AppComponent ]
+   declarations: [ 
+      AppComponent,
+      HeroComponent,
+      HeroDetailComponent
+    ],
+    providers: [
+       HeroService
+    ],
+    bootstrap: [ AppComponent ]
 })
 export class AppModule { }
