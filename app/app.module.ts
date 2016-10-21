3052b4020dd60ba0e23f208f2982b46240ec08a1
Prog2 <prog2@consysonline.com.br>
2016-10-21 16:08:52 -0200

    Tour of heroes v2 commit three

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,29 +1,20 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
-import { RouterModule }  from '@angular/router';
+import { NgModule }                  from '@angular/core';
+import { BrowserModule }             from '@angular/platform-browser';
+import { FormsModule }               from '@angular/forms';
 
-import { AppComponent }  from './app.component';
-import { DashboardComponent } from './dashboard.component'
-import { HeroesComponent } from './heroes.component';
-import { HeroDetailComponent } from './hero-detail.component';
-import { HeroService } from './hero.service';
+import { AppComponent }              from './app.component';
+import { DashboardComponent }        from './dashboard.component'
+import { HeroesComponent }           from './heroes.component';
+import { HeroDetailComponent }       from './hero-detail.component';
+import { HeroService }               from './hero.service';
+
+import { AppRoutingModule }          from './app-routing.module';
 
 @NgModule({
    imports: [
       BrowserModule,
       FormsModule,
-      RouterModule.forRoot([
-        {
-          path: 'heroes',
-          component: HeroesComponent
-        },
-        {
-          path: 'dashboard',
-          redirectTo: '/dashboard',
-          pathMatch: 'full'
-        }
-      ]) 
+      AppRoutingModule,
    ],
    declarations: [ 
       AppComponent,
@@ -31,9 +22,7 @@ import { HeroService } from './hero.service';
       HeroesComponent,
       HeroDetailComponent
     ],
-    providers: [
-       HeroService
-    ],
+    providers: [ HeroService ],
     bootstrap: [ AppComponent ]
 })
 export class AppModule { }
